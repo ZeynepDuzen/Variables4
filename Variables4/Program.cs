7115f0ebccff6e1d7533414ce69315f7e3261df1
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-15 11:12:41 +0300

    Proje dosyası ekle.

---
+++ Variables4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2, toplam, carpim, bolum, fark;
+            s1 = 10;
+            s2 = 2;
+            toplam = s1 + s2;
+            carpim = s1 * s2;
+            bolum = s1 / s2;
+            fark = s1 - s2;
+
+            Console.WriteLine("*** Aritmetik İslemler ***");
+            Console.WriteLine();
+            Console.WriteLine("Toplam : " + toplam);
+            Console.WriteLine("Bölüm : " + bolum);
+            Console.WriteLine("Çarpım : " + carpim);
+            Console.WriteLine("Fark : " + fark);
+            Console.WriteLine();
+            Console.WriteLine("*** Aritmetik İslemler ***");
+
+            Console.Read();
+        }
+    }
+}
